86731f24dcc5df3d92734b659f518752125d83d8
JGleslie017 <126440192+JGleslie017@users.noreply.github.com>
2023-03-17 20:52:54 -0600

    Add files via upload
    EJERCICIOS EN CLASE DESAFIOS

---
+++ desafios ejercicios/desafios ejercicios/desafio 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int main()
+            {
+                int primos_por_mostrar, n, d;
+                int es_primo;
+
+                printf("Cuantos primos desea mostrar: ");
+                scanf("%d", &primos_por_mostrar);
+
+                n = 2;
+                while (primos_por_mostrar > 0)
+                {
+
+                    /* determinar si n es primo */
+                    es_primo = 1;
+                    for (d = 2; d < n; ++d)
+                    {
+                        if (n % d == 0)
+                        {
+                            es_primo = 0;
+                            break;
+                        }
+                    }
+
+                    /* mostrar el numero
+                     * y actualizar el contador */
+                    if (es_primo)
+                    {
+                        printf("%d ", n);
+                        primos_por_mostrar--;
+                    }
+                    n++;
+                }
+
+                printf("\n");
+                return 0;
+            }
+        }
+    }
+}
